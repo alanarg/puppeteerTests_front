3754c2a70dd9149d381ec9dcfdc8759c4a7cc504
Alan Arguelho <alanarguelho3@outlook.com>
2021-03-03 08:36:15 -0400

    MUDANDO MAIN ROUTE

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
-import Argo from './login/index';
+// import Argo from './login/index';
 import Main from './GEDCORP/index';
 import PesquisaMsAdmin from './PESQUISAMS/PesquisaMsAdmin';
 import RedeSuas from './REDESUAS/index';
@@ -10,7 +10,7 @@ import RedeSuas from './REDESUAS/index';
 const Routes = ()=>(
     <BrowserRouter>
         <Switch>
-        <Route exact path="/" component={Argo}/>
+        <Route exact path="/" component={Main}/>
         <Route exact path="/GEDCORP" component={Main}/>
         <Route exact path="/REDESUAS" component={RedeSuas}/>
         <Route exact path="/PESQUISAMS_ADMIN" component={PesquisaMsAdmin}/>
