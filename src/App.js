6331eccc1f45f5e9e690c4745ff1c7951b9cef06
Alan Arguelho <alanarguelho3@outlook.com>
2021-03-17 14:21:56 -0400

    configurando .env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-//adicionando api, fica dentro da pasta services
-
-
 //esse arquivo style permite fazer umas pre formatações 
-
 import "./styles.css";
 
 //Cada componente deve ser importado
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // import $ from 'jquery';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `http://localhost:8080/`});
+const api = axios.create({ baseURL: `${process.env.REACT_APP_API_URL}`});
 
 export default api;
 
