54df872df688aaeee19326f3f2dde7680b5b778a
alanarg <alanarguelho2@gmail.com>
2021-04-06 10:04:00 -0400

    Erros material package

--- src/Regras/index.js
+++ src/Regras/index.js
@@ -1,8 +1,6 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
+import {Accordion,AccordionSummary,AccordionDetails} from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
