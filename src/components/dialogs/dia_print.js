89cd2fa236f53db7c4f681cc9c9a82aaf8aa5e37
Alan Arguelho <alanarguelho3@outlook.com>
2021-08-19 09:13:37 -0400

    print do capctcha

--- src/components/dialogs/dia_print.js
+++ src/components/dialogs/dia_print.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
+import { Form, Input} from 'antd';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import './styles.css';
-
+import api from '../../services/api';
 
 export default function ResponsiveDialog(props) {
   const [open, setOpen] = React.useState(false);
@@ -22,6 +23,19 @@ export default function ResponsiveDialog(props) {
     setOpen(false);
   };
   
+  const onFinish = async (values) => {
+    try {
+      console.log(values.captcha);
+      await api.post('/captcha',{"captcha":values.captcha},{headers: {'Content-Type': 'application/json'}});
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <div>
       <div>
@@ -35,6 +49,32 @@ export default function ResponsiveDialog(props) {
         onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
+        <DialogTitle id="responsive-dialog-title">{"CAPTCHA"}</DialogTitle>
+      <DialogContent >
+      <Form
+          name="basic"
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+        <Form.Item
+         label="Captcha"
+          name="captcha"
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+           Submit
+        </Button>
+        </Form.Item>
+      </Form>
+
+
+        <img src={`${process.env.REACT_APP_API_URL}/captcha.jpg`} width="100%"  alt="print"/>
+        
+       
+      </DialogContent>
         <DialogTitle id="responsive-dialog-title">{"Console Content"}</DialogTitle>
         <DialogContent >
           <img src={props.image} width="100%"  alt="print"/>
